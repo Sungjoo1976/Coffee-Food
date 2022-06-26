3b64d85a737cf441948b977935b019bc5755cc45
sungjoo yeom <darkysjnz@gmail.com>
2022-06-27 11:08:44 +1200

    Add files via upload

---
+++ Coffee Website (Assignment3)/Menu-page/Picture references.js
@@ -0,0 +1,4 @@
+https://www.pinterest.com/pin/310396599296107803/
+https://www.freepik.com/premium-vector/coffee-doodle-concept-sketch-illustration-about-coffee-time_7118743.htm
+
+https://www.xmple.com/wallpaper/red-stripes-yellow-white-lines-streaks-grey-brown--c5-696969-fa8072-eee8aa-cd853f-ffffff-l5-146-146-146-146-146-a-315-f-1-image/
